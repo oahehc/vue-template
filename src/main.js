d00001dea74d2d62b26080036e544c3f44738bf9
oahehc <oahehc@gmail.com>
2017-06-24 17:49:24 +0800

    adjust

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import router from './router';
-import store from './store/store';
+import router from '@/router';
+import store from '@/store/store';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 Vue.use(BootstrapVue);
 
